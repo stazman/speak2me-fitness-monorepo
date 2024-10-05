3fe8a24655c9c29aed153f95a8e3567ed6e46e1e
Chris DiStasio <cadistasio@gmail.com>
2024-10-05 21:53:48 +0000

    Reorganizes files, dirs acc to best practice, adds public/fonts dirs

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Adjust this if your frontend runs on a different URL
-    allow_credentials=True,
+    allow_origins=["http://localhost:3000"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
